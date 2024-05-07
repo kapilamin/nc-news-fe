ae188865e1dda8df6c8e845025942bf6dacfed43
Kapil Amin <kapilamin@Kapils-Laptop.local>
2024-05-07 12:25:00 +0100

    Task 7 completed

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,22 +1,53 @@
 import {Link} from 'react-router-dom';
 import {timeSinceDate} from '../utils';
+import { useState } from 'react';
+import { updateArticleVotes } from '../api';
 
 export const ArticleCard = ({article}) => {
+
+    const [articleVotes, setArticleVotes] = useState(article.votes);
+    const [err, setErr] = useState(null);
+
+    const handleArticleUpVote = () => {
+        setArticleVotes((currArticleVotes) => currArticleVotes +1)
+        setErr(null);
+        updateArticleVotes(article.article_id, 1)
+        .catch((err) => {
+            setArticleVotes((currArticleVotes) => currArticleVotes -1);
+            setErr("Voting error, please try again")
+        })
+    }
+
+    const handleArticleDownVote = () => {
+        setArticleVotes((currArticleVotes) => currArticleVotes -1)
+        updateArticleVotes(article.article_id, -1)
+        .catch((err) => {
+            setArticleVotes((currArticleVotes) => currArticleVotes +1);
+            setErr("Voting error, please try again")
+        })
+    }
+
     return (
         <article className="article-card">
-            <li>
-                <Link to={`/ncnews/${article.article_id}`}>
+            <Link to={`/ncnews/${article.article_id}`}>
+            <section id="article-card-content">
+                <li>
                     <h3>{article.title}</h3>
-                </Link>
-                <img className='article-image' src={article.article_img_url} alt="image of the article"/>
-                <p>Topic: {article.topic}</p>
-                <p>Author: {article.author}</p>
-                <p>Date created: {timeSinceDate(new Date(article.created_at))}</p>
-                <p>Number of Comments: {article.comment_count}</p>
-                <p>Votes: {article.votes}</p>
-                <button className='button-up'>Up vote</button>
-                <button className='button-down'>Down vote</button>
-            </li>
+                    <img className='article-image' src={article.article_img_url} alt="image of the article"/>
+                    <p>Topic: {article.topic}</p>
+                    <p>Author: {article.author}</p>
+                    <p>Date created: {timeSinceDate(new Date(article.created_at))}</p>
+                    <p>Number of Comments: {article.comment_count}</p>
+                    <p>Votes: {articleVotes}</p>
+                </li>
+            </section>
+            </Link>
+                {err ? <p>{err}</p> : null}
+            <section className="article-card-buttons">    
+                    <button className='button-up' onClick = {handleArticleUpVote} id = "article-up-vote-button">Up vote</button>
+                    <button className='button-down' onClick={handleArticleDownVote} id="article-down-vote-button">Down vote</button>
+            </section>
+
         </article>
     )
 }
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,8 +3,8 @@ import { timeSinceDate } from "../utils"
 export const CommentCard = ({comment}) => {
     return(
         <article className="comment-card">
-            <p>Date posted: {timeSinceDate(new Date(comment.created_at))}</p>
             <p>By: {comment.author}</p>
+            <p>Date posted: {timeSinceDate(new Date(comment.created_at))}</p>
             <p>{comment.body}</p>
             <p>Votes: {comment.votes}</p>
         </article>
