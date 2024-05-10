a1c51cea8dd4bdc150f87609e5a58c5ec1f96829
Kapil <146853684+kapilamin@users.noreply.github.com>
2024-05-10 13:04:43 +0100

    removed icons as not showing on app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,10 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <nav id="footer-links-container">
-        <a href="https://www.x.com" target="_blank"><img src="../icons/twitter.png"></img><p>X</p></a>
-        <a href="https://www.instagram.com" target="blank"><img src="../icons/instagram.png"></img><p>Instagram</p></a>
+        <a href="https://www.x.com" target="_blank"><p>X</p></a>
+        <a href="https://www.instagram.com" target="blank"><p>Instagram</p></a>
       </nav>
     </footer>
   );
 };
+
