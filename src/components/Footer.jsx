95161f75869ca405e3a95d38bd96ebcc8da16fac
Kapil <146853684+kapilamin@users.noreply.github.com>
2024-05-10 12:53:21 +0100

    updated styling to improve accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <nav id="footer-links-container">
-        <a href="https://www.x.com" target="_blank"><img src="../icons/twitter.png"></img></a>
-        <a href="https://www.instagram.com" target="blank"><img src="../icons/instagram.png"></img></a>
+        <a href="https://www.x.com" target="_blank"><img src="../icons/twitter.png"></img><p>X</p></a>
+        <a href="https://www.instagram.com" target="blank"><img src="../icons/instagram.png"></img><p>Instagram</p></a>
       </nav>
     </footer>
   );
