5e0ad52692ddf352285bd978de9a5c27531ecc91
Kapil <146853684+kapilamin@users.noreply.github.com>
2024-05-10 15:08:39 +0100

    added more styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <nav id="footer-links-container">
-        <a href="https://www.x.com" target="_blank"><p>X</p></a>
-        <a href="https://www.instagram.com" target="blank"><p>Instagram</p></a>
+        <a href="https://www.x.com" target="_blank"><img src="./x.png"></img></a>
+        <a href="https://www.instagram.com" target="blank"><img src="./instagram.png"></img></a>
       </nav>
     </footer>
   );
